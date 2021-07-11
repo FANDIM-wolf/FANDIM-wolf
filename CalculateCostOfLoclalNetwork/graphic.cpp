d4540043c5970e5448797b889a3c52a94877324c
Mikhail Vanderlip <63235817+FANDIM-wolf@users.noreply.github.com>
2021-07-11 17:41:09 +0300

    Add files via upload

--- CalculateCostOfLoclalNetwork/graphic.cpp
+++ CalculateCostOfLoclalNetwork/graphic.cpp
@@ -40,9 +40,11 @@ Graphic::~Graphic()
 
 void Graphic::on_pushButton_clicked()
 {
+        //set min value to find min price
+
         int N = 231;
         QVector<double> x(N), y(N); //Array of coordinates of points
-
+        int min = 0 ;
         //Process data for graphic
             int i=0;
             x[0]=2020;
@@ -62,8 +64,18 @@ void Graphic::on_pushButton_clicked()
                  int salary = query->value(1).toInt();
                   y[i]= salary;
                   x[i]=1000+x[i];
-               }
+                  if(i == 0){
+                      min = x[i];
+                  }
+            }
 
+        //get min value
+        for(int j = 0 ; j<170; j++){
+            if(x[j] < min ){
+                min=x[i];
+            }
+
+        }
         ui->widget->clearGraphs();//Если нужно, но очищаем все графики
         //Добавляем один график в widget
         ui->widget->addGraph();
@@ -77,6 +89,12 @@ void Graphic::on_pushButton_clicked()
         //Set field where we show points
         ui->widget->xAxis->setRange(1, 10);//for Ox
         ui->widget->yAxis->setRange(1, 10);//for Oy
+
+
+        //set min value in label
+
+        QString min_value = QString::number(min);//from string to int
+        ui->label_2->setText(min_value); // set price;
 
         //И перерисуем график на нашем widget
         ui->widget->replot();
