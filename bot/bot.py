69e21982196fea2a4a9de86503a9c8c1859996d9
Mikhail Vanderlip <63235817+FANDIM-wolf@users.noreply.github.com>
2021-07-22 14:47:41 +0300

    Add files via upload

--- bot/bot.py
+++ bot/bot.py
@@ -1,40 +1,68 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
 import datetime
+import time
+import os
+from choice import robot
 
 
 now = datetime.datetime.now()
 bot_age = now.year - 2021
-tok="664829dd2a3c10fa21cd9e1f572cfc82bb9bc10b7793fccc286e4ac7a6613861a11eea66081cd3282bfe2"
 
-vk_session = vk_api.VkApi(token ="664829dd2a3c10fa21cd9e1f572cfc82bb9bc10b7793fccc286e4ac7a6613861a11eea66081cd3282bfe2" )
-longpoll = VkBotLongPoll(vk_session, 205925214)
+vk_session = vk_api.VkApi(token ="token" )
+longpoll = VkBotLongPoll(vk_session, id_ssession)
 
 def sender(id,text):
 	vk_session.method('messages.send',{'chat_id':id,'message':text,'random_id':0})
 
-for event in longpoll.listen():
-	if event.type == VkBotEventType.MESSAGE_NEW:
+#def send_msg(peer_id: int, message: str, attachment: str = ""):
+#    return vk_.method("messages.send", {**locals(), "random_id": 0})
 
-		
+def job():
+    print("I'm working...")
 
-		if event.from_chat:
-			id = event.chat_id
-			msg = event.obj.message['text'].lower()
-			bot_age = now.year - 2021
-			try:
-				dey = event.message.action['type']
-				invite_id  = event.message.action['member_id']
-			except:
-				dey  = ''
-				invite_id = -100	
-			if dey == "chat_invite_user":
-				sender(id,f'Hi , @{invite_id} , read rules of chat or ask admin about it.')
+#create object of vk bot
+vk_bot = robot("Helga")
 
 
-			if msg == "hello" :
-				sender(id,"hello")
-			if msg == "how many years do you exist":
-				text = str(bot_age)
-				sender(id,text)
-		
+
+while True:
+	
+	
+
+	for event in longpoll.listen():
+		if event.type == VkBotEventType.MESSAGE_NEW:
+
+			
+
+
+			if event.from_chat:
+				id = event.chat_id
+				msg = event.obj.message['text'].lower()
+				bot_age = now.year - 2021
+				
+				try:
+					
+					dey = event.message.action['type']
+					invite_id  = event.message.action['member_id']
+				except:
+					dey  = ''
+					invite_id = -100	
+				if dey == "chat_invite_user":
+					sender(id,f'Hi , @{invite_id} , read rules of chat or ask admin about it.')
+
+
+				if msg == "hello" :
+					sender(id,"hello")
+				if msg == "how many years do you exist":
+					text = str(bot_age)
+					sender(id,text)
+				if msg == "random song":
+					text = vk_bot.random_song()
+					sender(id,text)	
+
+				
+					
+			
+				
+				
--- bot/choice.py
+++ bot/choice.py
@@ -1,13 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
+import random 
 
+global random_song 
+
+data_of_songs={
+    'random_song':""
+}
 
 url = "https://www.esquire.com/entertainment/music/g36135549/best-songs-2021/"
+list_songs = []
 
-req =  requests.get(url)
-src =  BeautifulSoup(req.text,'lxml')
-songs = src.find_all('span',class_='listicle-slide-hed-text')
 
-print(songs)
-for song in songs:
-    print(song.text)
+
+class robot:
+#choice random song 
+    name = "None"
+    # constructor
+    def __init__(self, name):
+        self.name = name  # set name
+    @staticmethod    
+    def random_song():
+        
+
+        req =  requests.get(url)
+        src =  BeautifulSoup(req.text,'lxml')
+        songs = src.find_all('span',class_='listicle-slide-hed-text')
+
+        for el in songs:
+            list_songs.append(el.text)
+        random_song = random.choice(list_songs)
+        
+        return random_song
+        
+
+
+
+print()
