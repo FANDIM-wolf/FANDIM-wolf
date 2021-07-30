07ba6869e4c8d62a66d2bd7b923b70ba8ea2b90f
Mikhail Vanderlip <63235817+FANDIM-wolf@users.noreply.github.com>
2021-07-30 15:59:40 +0300

    Add files via upload

--- AuthPathCypther/mainwindow.cpp
+++ AuthPathCypther/mainwindow.cpp
@@ -1,8 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-//result of void function
-QString result_string_cypher;
-
+#include "basic.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,38 +16,6 @@ MainWindow::~MainWindow()
 
 //cypher function
 
-QString cypher_text(QString input_text_to_cypher_it){
-
-    for(int i = 0; i<=input_text_to_cypher_it.length() ; i++){
-            if(input_text_to_cypher_it[i] ==
-                'a' ){
-
-                 input_text_to_cypher_it[i] = 'n';
-
-
-            }
-
-    }
-    return input_text_to_cypher_it;
-
-}
-//uncypher function
-
-QString uncypher_text(QString input_text_to_uncypher_it){
-
-    for(int i = 0; i<=input_text_to_uncypher_it.length() ; i++){
-            if(input_text_to_uncypher_it[i] ==
-                'n' ){
-
-
-                 input_text_to_uncypher_it[i] = 'a';
-                 //add valut to result
-
-            }
-
-    }
-    return input_text_to_uncypher_it;
-}
 void MainWindow::on_pushButton_clicked()
 {   //get data
     QString input = ui->textEdit->toPlainText();
