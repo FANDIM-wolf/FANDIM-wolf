b30798c0a1358d78657f0afc64f4fb80cc45f494
Mikhail Vanderlip <63235817+FANDIM-wolf@users.noreply.github.com>
2021-08-02 18:51:53 +0300

    Add files via upload

--- AuthPathCypther/basic.h
+++ AuthPathCypther/basic.h
@@ -8,57 +8,64 @@ QString cypher_text(QString input_text_to_cypher_it){
     for(int i = 0; i< input_text_to_cypher_it.length() ; i++){
             if(input_text_to_cypher_it[i] ==
                 'a' ){
-                 input_text_to_cypher[i] = '-';
+                 input_text_to_cypher[i] = 'c';
             }
             if(input_text_to_cypher_it[i] ==
                 'b' ){
-                 input_text_to_cypher_it[i] = '+';
+                 input_text_to_cypher[i] = '+';
             }
             if(input_text_to_cypher_it[i] ==
                 'c' ){
-                 input_text_to_cypher_it[i] = 'a';
+                 input_text_to_cypher[i] = 'a';
             }
             if(input_text_to_cypher_it[i] ==
                 'd' ){
-                 input_text_to_cypher_it[i] = 'e';
+                 input_text_to_cypher[i] = 'e';
             }
             if(input_text_to_cypher_it[i] ==
                 'e' ){
-                 input_text_to_cypher_it[i] = 'd';
+                 input_text_to_cypher[i] = 'd';
             }
             if(input_text_to_cypher_it[i] ==
                 'f' ){
-                 input_text_to_cypher_it[i] = 'g';
+                 input_text_to_cypher[i] = 'g';
             }
             if(input_text_to_cypher_it[i] ==
                 'g' ){
-                 input_text_to_cypher_it[i] = 'f';
+                 input_text_to_cypher[i] = 'f';
             }
             if(input_text_to_cypher_it[i] ==
                 'h' ){
-                 input_text_to_cypher_it[i] = 'j';
+                 input_text_to_cypher[i] = 'j';
             }
             if(input_text_to_cypher_it[i] ==
                 'i' ){
-                 input_text_to_cypher_it[i] = 'k';
+                 input_text_to_cypher[i] = 'k';
             }
             if(input_text_to_cypher_it[i] ==
                 'j' ){
-                 input_text_to_cypher_it[i] = 'h';
+                 input_text_to_cypher[i] = 'h';
             }
             if(input_text_to_cypher_it[i] ==
                 'k' ){
-                 input_text_to_cypher_it[i] = 'i';
+                 input_text_to_cypher[i] = 'i';
             }
             if(input_text_to_cypher_it[i] ==
                 'l' ){
-                 input_text_to_cypher_it[i] = 'm';
+                 input_text_to_cypher[i] = 'm';
             }
             if(input_text_to_cypher_it[i] ==
                 'm' ){
-                 input_text_to_cypher_it[i] = 'l';
+                 input_text_to_cypher[i] = 'l';
             }
-
+            if(input_text_to_cypher_it[i] ==
+                'n' ){
+                 input_text_to_cypher[i] = 'p';
+            }
+            if(input_text_to_cypher_it[i] ==
+                'o' ){
+                 input_text_to_cypher[i] = 'n';
+            }
 
 
     }
